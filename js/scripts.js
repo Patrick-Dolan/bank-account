64ff8cd14a14b8245d389ffeb0872ed7f9fce74f
Patrick Dolan <dolanp1992@gmail.com>
2021-11-29 20:09:47 -0800

    add ability for deposit and withdrawals to change account balance
    Ryan Rendon: Co-authored-by: Ryan Rendon <Rendon.S.Ryan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function Account(name, balance) {
 Account.prototype.withdraw = function(amount) {
   this.balance -= amount;
   if (this.balance < 0 ) {
-    this.balance += amount;
+    this.balance -= amount;
     return "Insufficent Funds!";
   }
 }
@@ -14,19 +14,29 @@ Account.prototype.withdraw = function(amount) {
 Account.prototype.deposit = function(amount) {
   this.balance += amount;
 }
-
+let newAccount = new Account("Name", 0);
 
 $(document).ready(function() {
   $("#registerForm").submit(function (event) {
     event.preventDefault();
     const name = $("input#name").val();
     const balance = parseInt($("input#initialDeposit").val());
-    let newAccount = new Account(name, balance);
+    newAccount = new Account(name, balance);
+    $(".accountBalance").html(newAccount.balance);
     console.log(newAccount);
   });
-  $("#transaction").submit(function (event) {
+  $("#fundsForm").submit(function (event) {
     event.preventDefault();
-
+    let transaction = parseInt($("select#transaction").val());
+    const amount = parseInt($("input#amount").val());
+    console.log(transaction);
+    if (transaction === 1) {
+      newAccount.deposit(amount);
+      $(".accountBalance").html(newAccount.balance);
+    } else {
+      newAccount.withdraw(amount);
+      $(".accountBalance").html(newAccount.balance);
+    }
   });
 });
 
